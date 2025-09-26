7960a4e48d682bd2de629402731d65ee98a3191a
EtoOnAmill <alezardo12@gmail.com>
2025-09-26 20:20:40 +0200

    moved the query creation to scheme

--- src/taggy.scm
+++ src/taggy.scm
@@ -1,9 +1,32 @@
 (define-module (taggy)
-    #:export (hi))
+    #:export (hi db_add add_query))
 
-(load-extension "guile-db" "init_module")
+(load-extension "./build/guile_db.so" "init_module")
 
-(define hi (write-string "BYE"))
+(define hi (lambda () (write "BYE\n")))
 
-(define db_add (lambda (db rows . values)) 
-    )
+(define db_add (lambda (db rows . values)
+    (cond
+        ((nil? values) #t)
+        ((nil? (car values)) #t)
+        ((pair? values)
+            (if (add_query db rows (list->vector (car values)))
+                (apply db_add db rows (cdr values))
+                values))
+        (#t (list db rows values)))))
+
+(define (symvec->str v)
+    (string-append "(" (string-join (map symbol->string (vector->list v)) ",") ")" ))
+(define (strvec->str v)
+    (string-append "(" (string-join (vector->list v) ",") ")" ))
+
+(define (add_query db rows values)
+    (cond
+        ((not (or
+            (string? db)
+            (vector? rows)
+            (vector? values)
+            (eq? (vector-length values) (vector-length rows)))) #f)
+        (#t (begin
+                   (write (string-append "INSERT INTO " db (symvec->str rows) " VALUES " (strvec->str values)))
+                   #t))))
