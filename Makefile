65faff2e558ba5ff03528308cb3b1b097f456287
EtoOnAmill <alezardo12@gmail.com>
2025-09-28 19:49:07 +0200

    Rename to something more unique for github

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 SRCD=./src
 OBJD=./build
-OBJ=$(OBJD)/taggy.o $(OBJD)/guile_db.so $(OBJD)/guile_db.o
-BINNAME=taggy
+OBJ=$(OBJD)/categ.o $(OBJD)/guile_db.so $(OBJD)/guile_db.o
+BINNAME=categ
 
 CC=gcc
 GC=guild
@@ -11,13 +11,13 @@ GFLAGS=compile
 LIBGUILE_FLAG=`pkg-config --cflags guile-3.0` -c -fPIC
 
 all : $(OBJ)
-	$(CC) -o $(OBJD)/taggy $(OBJD)/taggy.o `pkg-config guile-3.0 --libs`
+	$(CC) -o $(OBJD)/categ $(OBJD)/categ.o `pkg-config guile-3.0 --libs`
 
-$(OBJD)/taggy.o : $(SRCD)/taggy.c
-	$(CC) $(CFLAGS) -o $(OBJD)/taggy.o $(SRCD)/taggy.c
+$(OBJD)/categ.o : $(SRCD)/categ.c
+	$(CC) $(CFLAGS) -o $(OBJD)/categ.o $(SRCD)/categ.c
 
-$(OBJD)/taggy.go : $(SRCD)/taggy.scm
-	$(GC) $(GFLAGS) -o $(OBJD)/taggy.go $(SRCD)/taggy.scm
+$(OBJD)/categ.go : $(SRCD)/categ.scm
+	$(GC) $(GFLAGS) -o $(OBJD)/categ.go $(SRCD)/categ.scm
 
 $(OBJD)/guile_db.so : $(OBJD)/guile_db.o
 	$(CC) -shared -o $(OBJD)/guile_db.so $(OBJD)/guile_db.o -lsqlite3
@@ -27,4 +27,4 @@ $(OBJD)/guile_db.o : $(SRCD)/guile_db.c
 
 .PHONY : clean
 clean :
-	rm $(OBJD)/taggy $(OBJ)
+	rm $(OBJD)/categ $(OBJ)
---
+++ src/categ.scm
@@ -0,0 +1,92 @@
+(define-module (caria)
+    #:export (hi table-add table-remove table-select))
+
+(load-extension "./build/guile_db.so" "init_module")
+
+(define hi (lambda () (display "BYE\n")))
+
+
+
+(define* (table-select table rows #:key join . filters)
+    (let*
+         ((join-str (if join join " AND "))
+          (filter-no-key
+              (if join
+                   (delv join (delv #:join filters))
+                   filters))
+          (filter-str
+              (cond
+              ((nil? filter-no-key) "TRUE")
+              ((pair? filter-no-key) (apply string-append (intersperse join-str filter-no-key))))))
+    (select_query table rows filter-str)))
+
+(define (select_query table rows filter)
+    (if (and 
+            (string? table) 
+            (vector? rows)
+            (string? filter))
+        (let
+            ((query (string-append "SELECT " (symvec->str rows) " FROM " table " WHERE " filter "\n")))
+            (begin (display query) (guiledb-select query)))
+        #f ))
+
+(define (table-add table rows . values)
+    (cond
+        ((nil? values) #t)
+        ((pair? values)
+            (if (add_query table rows (list->vector (car values)))
+                (apply table-add table rows (cdr values)) ; without apply with every recursion the 'values' variadic argument gets wrapped in an extra list instead of using the list itself
+                values))
+        (#t (list table rows values))))
+
+(define (add_query table rows values)
+    (if (and
+            (string? table)
+            (vector? rows)
+            (vector? values)
+            (eq? (vector-length values) (vector-length rows)))
+        (let 
+            ((query (string-append "INSERT INTO " table "("(symvec->str rows) ") VALUES " (strvec->str values) "\n")))
+            (begin
+                (display query)
+                (guiledb-query query)))
+        #f ))
+
+
+
+(define (table-remove table . selectors)
+    (cond
+        ((nil? selectors) #t)
+        ((pair? selectors)
+            (if (remove_query table (car selectors))
+                (apply table-remove (cons table (cdr selectors)))
+                selectors))
+        (#t (list table selectors))))
+
+(define (remove_query table selector)
+    (if (and (string? table) (string? selector))
+        (let 
+            ((query (string-append "DELETE FROM " table " WHERE " selector "\n")))
+            (begin
+                (display query)
+                (guiledb-query query)))
+        #f ))
+
+
+
+
+(define (symvec->str v)
+    (if (eq? 0 (vector-length v))
+        "*"
+        (string-append (string-join (map symbol->string (vector->list v)) ",") )))
+(define (strvec->str v)
+    (string-append "(" (string-join (vector->list v) ",") ")" ))
+(define (intersperse operator operands)
+;    (display operator)
+;    (display operands)
+;    (display "\n______\n")
+    (cond
+        ((nil? operands) operands)
+        ((nil? (cdr operands)) operands)
+        ((pair? operands)
+            (cons (car operands) (cons operator (intersperse operator (cdr operands)))))))
